0e842673a897f99aaaaeda4732e3c581c58211cc
azacdev <azacdev@gmail.com>
2024-01-09 23:01:50 +0100

    test

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,6 +1,3 @@
-// Use 'use strict' instead of 'use server' for strict mode
-("use strict");
-
 import { NextResponse } from "next/server";
 import prismadb from "@lib/prismadb";
 
@@ -8,12 +5,10 @@ const crypto = require("crypto");
 
 export async function POST(req: Request) {
   const body = await req.json();
-  console.log('requestBody', body)
-  const headers = req.headers;
-  const secret = process.env.PAYSTACK_SECRET_TEST_KEY;
-  const metadata = body.data.metadata;
-
-  console.log(metadata);
+  console.log("requestBody", body);
+  // const headers = req.headers;
+  // const secret = process.env.PAYSTACK_SECRET_TEST_KEY;
+  // const metadata = body.data.metadata;
 
   // const hash = crypto
   //   .createHmac("sha512", secret)
@@ -21,51 +16,50 @@ export async function POST(req: Request) {
   //   .digest("hex");
   // const signature = headers.get("x-paystack-signature");
 
-  // if (hash !== signature) {
-    // Retrieve the request's body
+  // if (hash == signature) {
+  //   // Retrieve the request's body
+  //   const eventType = body.event;
+  //   const chargeData = body.data;
+  //   const status = chargeData.status;
 
-    const eventType = body.event;
-    const chargeData = body.data;
-    const status = chargeData.status;
+  //   if (eventType === "charge.success") {
+  //     if (status === "success") {
+  //       // Success! Confirm the customer's payment
+  //       const order = await prismadb.order.update({
+  //         where: {
+  //           id: metadata?.orderId,
+  //         },
+  //         data: {
+  //           isPaid: true,
+  //           address: metadata.state,
+  //           phone: metadata?.phone || "",
+  //         },
+  //         include: {
+  //           orderItems: true,
+  //         },
+  //       });
 
-    // if (eventType === "charge.success") {
-    //   if (status === "success") {
-    //     // Success! Confirm the customer's payment
-    //     const order = await prismadb.order.update({
-    //       where: {
-    //         id: metadata?.orderId,
-    //       },
-    //       data: {
-    //         isPaid: true,
-    //         address: metadata.state,
-    //         phone: metadata?.phone || "",
-    //       },
-    //       include: {
-    //         orderItems: true,
-    //       },
-    //     });
+  //       const productIds = order.orderItems.map(
+  //         (orderItems) => orderItems.productId
+  //       );
 
-    //     const productIds = order.orderItems.map(
-    //       (orderItems) => orderItems.productId
-    //     );
+  //       await prismadb.product.updateMany({
+  //         where: {
+  //           id: {
+  //             in: [...productIds],
+  //           },
+  //         },
+  //         data: {
+  //           isArchived: true,
+  //         },
+  //       });
 
-    //     await prismadb.product.updateMany({
-    //       where: {
-    //         id: {
-    //           in: [...productIds],
-    //         },
-    //       },
-    //       data: {
-    //         isArchived: true,
-    //       },
-    //     });
-
-    //     return NextResponse.json(
-    //       { message: "Payment unsuccessful" },
-    //       { status: 400 }
-    //     );
-    //   }
-  //   // }
+  //       return NextResponse.json(
+  //         { message: "Payment unsuccessful" },
+  //         { status: 400 }
+  //       );
+  //     }
+  //   }
   // } else {
   //   return NextResponse.json(
   //     { message: "This request isn't from Paystack" },
