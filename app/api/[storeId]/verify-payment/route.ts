918a5e6db8a6d3961edd528060ef8ebfae3350c4
azacdev <azacdev@gmail.com>
2024-01-22 02:09:09 +0100

    cors43

--- app/api/[storeId]/verify-payment/route.ts
+++ app/api/[storeId]/verify-payment/route.ts
@@ -1,44 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-  const origin = request.headers.get("Origin");
-
-  // @ts-ignore
-  if (origin && allowedOrigins.includes(origin)) {
-    const responseHeaders = new Headers();
-    responseHeaders.append(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS"
-    );
-    responseHeaders.append(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    responseHeaders.append("Access-Control-Allow-Credentials", "true");
-    responseHeaders.append("Access-Control-Allow-Origin", origin);
-
-    if (request.method === "OPTIONS") {
-      return new Response(null, {
-        status: 200,
-        headers: responseHeaders,
-      });
-    } else {
-      const response = NextResponse.next({});
-      response.headers.set(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS"
-      );
-      response.headers.set(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization"
-      );
-      response.headers.set("Access-Control-Allow-Credentials", "true");
-      response.headers.set("Access-Control-Allow-Origin", origin);
-
-      return response;
-    }
-  }
-}
+import { NextResponse } from "next/server";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
