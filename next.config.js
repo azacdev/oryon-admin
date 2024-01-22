0bafb9023246678c1a6e18b56a058c0b78f186fd
azacdev <azacdev@gmail.com>
2024-01-22 01:45:22 +0100

    cors-1

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
@@ -22,7 +23,7 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Authorization, Content-Type, Date, X-Api-Version",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
