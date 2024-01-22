53c3e313aad1aa7585bbf9566e8710e28a469580
azacdev <azacdev@gmail.com>
2024-01-22 02:05:30 +0100

    cors42

--- app/api/[storeId]/verify-payment/route.ts
+++ app/api/[storeId]/verify-payment/route.ts
@@ -1,4 +1,44 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const origin = request.headers.get("Origin");
+
+  // @ts-ignore
+  if (origin && allowedOrigins.includes(origin)) {
+    const responseHeaders = new Headers();
+    responseHeaders.append(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS"
+    );
+    responseHeaders.append(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    responseHeaders.append("Access-Control-Allow-Credentials", "true");
+    responseHeaders.append("Access-Control-Allow-Origin", origin);
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 200,
+        headers: responseHeaders,
+      });
+    } else {
+      const response = NextResponse.next({});
+      response.headers.set(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS"
+      );
+      response.headers.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      response.headers.set("Access-Control-Allow-Credentials", "true");
+      response.headers.set("Access-Control-Allow-Origin", origin);
+
+      return response;
+    }
+  }
+}
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -17,64 +57,52 @@ export async function GET(
   const { searchParams } = new URL(req.url);
   const reference = searchParams.get("reference");
 
-  console.log(reference);
+  try {
+    if (!params.storeId) {
+      return new NextResponse("StoreId is required", { status: 400 });
+    }
 
-  return new Response("Verification successful", {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  });
-  // try {
-  //   if (!params.storeId) {
-  //     return new NextResponse("StoreId is required", { status: 400 });
-  //   }
+    if (!reference) {
+      return new NextResponse("Transaction reference is missing", {
+        status: 400,
+      });
+    }
+    const verifyUrl = `https://api.paystack.co/transaction/verify/${reference}`;
+    console.log(verifyUrl);
 
-  //   if (!reference) {
-  //     return new NextResponse("Transaction reference is missing", {
-  //       status: 400,
-  //     });
-  //   }
-  //   const verifyUrl = `https://api.paystack.co/transaction/verify/${reference}`;
-  //   console.log(verifyUrl);
+    const verifyResponse = await fetch(verifyUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_TEST_KEY}`,
+      },
+    });
 
-  //   const verifyResponse = await fetch(verifyUrl, {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: `Bearer ${process.env.PAYSTACK_SECRET_TEST_KEY}`,
-  //     },
-  //   });
+    if (!verifyResponse.ok) {
+      console.log(verifyResponse.status);
+      throw new Error(`Paystack API returned status ${verifyResponse.status}`);
+    }
 
-  //   if (!verifyResponse.ok) {
-  //     console.log(verifyResponse.status);
-  //     throw new Error(`Paystack API returned status ${verifyResponse.status}`);
-  //   }
+    const verifyResult = await verifyResponse.json();
 
-  //   const verifyResult = await verifyResponse.json();
-
-  //   // Check the status from the verification result
-  //   if (verifyResult.status && verifyResult.data.status === "success") {
-  //     return new NextResponse("Verification successful", {
-  //       status: 200,
-  //       headers: corsHeaders,
-  //     });
-  //   } else {
-  //     // Handle unsuccessful verification
-  //     return new NextResponse("Verification failed", {
-  //       status: 400,
-  //       headers: corsHeaders,
-  //     });
-  //   }
-  // } catch (error: any) {
-  //   console.error("Error:", error);
-  //   return new NextResponse(
-  //     JSON.stringify({ error: "An error has occurred" }),
-  //     {
-  //       status: 500,
-  //       headers: corsHeaders,
-  //     }
-  //   );
-  // }
+    if (verifyResult.status && verifyResult.data.status === "success") {
+      return new NextResponse("Verification successful", {
+        status: 200,
+        headers: corsHeaders,
+      });
+    } else {
+      return new NextResponse("Verification failed", {
+        status: 400,
+        headers: corsHeaders,
+      });
+    }
+  } catch (error: any) {
+    console.error("Error:", error);
+    return new NextResponse(
+      JSON.stringify({ error: "An error has occurred" }),
+      {
+        status: 500,
+        headers: corsHeaders,
+      }
+    );
+  }
 }
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -10,3 +11,31 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+export function middleware(request: NextRequest) {
+  const origin = request.headers.get('Origin')
+
+  // @ts-ignore
+  if (origin && allowedOrigins.includes(origin)) {
+    const responseHeaders = new Headers()
+    responseHeaders.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    responseHeaders.append('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    responseHeaders.append('Access-Control-Allow-Credentials', 'true')
+    responseHeaders.append('Access-Control-Allow-Origin', origin)
+
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        status: 200,
+        headers: responseHeaders,
+      })
+    } else {
+      const response = NextResponse.next({})
+      response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+      response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+      response.headers.set('Access-Control-Allow-Credentials', 'true')
+      response.headers.set('Access-Control-Allow-Origin', origin)
+
+      return response
+    }
+  }
+}
