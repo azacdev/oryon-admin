f7eaf1bb58f8323f2eee416512e8bf1a21582fbe
azacdev <azacdev@gmail.com>
2024-01-22 02:10:11 +0100

    cors44

--- next.config.js
+++ next.config.js
@@ -11,14 +11,13 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
